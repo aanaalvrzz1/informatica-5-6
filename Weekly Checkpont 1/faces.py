44022706cf252a4312bfb3eb71a61159eabdf210
aanaalvrzz1 <annesophia060909@gmail.com>
2026-08-14 17:15:17 +0000

    weekly checkpoint 1

---
+++ Weekly Checkpont 1/faces.py
@@ -0,0 +1,12 @@
+def main():
+
+    message=input("type a message:")
+    print(message.replace(":)","🙂"))
+
+    print()
+
+    message=input("type a message:")
+    print(message.replace(":(","🙁"))
+
+if __name__== "__main__":
+    main()
--- Weekly Checkpont 1/story.py
+++ Weekly Checkpont 1/story.py
@@ -14,17 +14,19 @@ def main():
     # #Formatted String
     # print(f"Hello {planet}")
 
-    name= input("what is your name? ")
-    color=input("Tell me a color: ")
-    adj=input("Tell me an adjective: ")
-    goal=input("A goal you would like to achieve: ")
+    name= input("what is your name? ").title().strip()
+    color=input("Tell me a color: ").lower().strip()
+    adj=input("Tell me an adjective: ").lower().strip()
+    goal=input("A goal you would like to achieve: ").lower().strip()
 
     print()
     print("Hello", name)
     print()
-    print("this is your story: ")
+    print("this is your story:")
     print(f"At dawn the sky turned {color}, and the air felt {adj}. I decided today I will finally {goal}.")
+    print()
 
+    print(f"At dawn the sky turned {color}, and the air felt {adj}. I decided today I will finally {goal}.".upper())
 
 
 
