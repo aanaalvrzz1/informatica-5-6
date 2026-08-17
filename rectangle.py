1ec80780486e3739d9a6631525b11cf100d55b53
aanaalvrzz1 <annesophia060909@gmail.com>
2026-08-17 19:39:29 +0000

    rectangle

---
+++ rectangle.py
@@ -0,0 +1,20 @@
+def main():
+    width = int(input("Enter the width of the rectangle:"))
+    print("O"*width)
+    print("O"*width)
+    print("O"*width)
+    print("O"*width)
+    print("O"*width)
+
+    perimeter = (width + 5)*2
+    print("this is your perimeter:", perimeter)
+    area = (width*5)
+    print("this is your area:",area)
+    diagonal = 
+
+
+
+
+
+if __name__== "__main__":
+    main()
