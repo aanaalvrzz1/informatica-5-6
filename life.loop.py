db1c7df6da65295e79b86c96fd4dfa76f7a9ff31
aanaalvrzz1 <annesophia060909@gmail.com>
2026-09-07 19:40:32 +0000

    advance of life loop

---
+++ life.loop.py
@@ -0,0 +1,23 @@
+import time
+def main():
+
+    print("welcome to remember to drink your daily water! ")
+    print("")
+    person = input("do you consider yourself a sedentary person, Moderate activity person or a sportsman? ")
+    weight = float(input("what is your weight? "))
+
+    if person == sedentary person:
+        weight * 
+
+
+
+    reminder = 5
+
+    while reminder > 0:
+        print("drink water!")
+        time.sleep(10)
+        reminder = reminder - 1
+
+
+if __name__=="__main__":
+    main()
