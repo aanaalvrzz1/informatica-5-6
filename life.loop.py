b8b0654a5eb7edc4ff7acd94c8b53cc00e7f4186
aanaalvrzz1 <annesophia060909@gmail.com>
2026-09-08 16:41:45 +0000

    life loop complete

--- life.loop.py
+++ life.loop.py
@@ -3,19 +3,30 @@ def main():
 
     print("welcome to remember to drink your daily water! ")
     print("")
-    person = input("do you consider yourself a sedentary person, Moderate activity person or a sportsman? ")
-    weight = float(input("what is your weight? "))
+    person = input("do you consider yourself a sedentary, Moderate or a sportsman? ").strip().lower()
+    weight = float(input("what is your weight(kg)? "))
 
-    if person == sedentary person:
-        weight * 
+    if person == "sedentary":
+        total = weight * 30
+
+    elif person == "moderate":
+        total = weight * 35
+
+    elif person == "sportsman":
+        total = weight * 40
+
+    else:
+        print("that is not an option")
+
+    print(f"you need to drink {total} ml every day " )
+    print(" timer is set for every hour (8 total)")
+    print(f"drink {total/5} ml every hour")
 
 
-
-    reminder = 5
-
+    reminder = 8
     while reminder > 0:
         print("drink water!")
-        time.sleep(10)
+        time.sleep(2)
         reminder = reminder - 1
 
 
