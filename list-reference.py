7af90a1de4d011d3315dac35d6683673da1776bd
aanaalvrzz1 <annesophia060909@gmail.com>
2026-09-10 20:20:45 +0000

    work advance

---
+++ list-reference.py
@@ -0,0 +1,14 @@
+def main():
+
+    #.append()
+    fruits = ["apple" , "orange" , "grapes"]
+    fruits.append("banana")
+    print(fruits)
+
+    #.insert()
+    fruits1 = ["apple" , "orange" , "grapes"]
+    fruits1.insert(2,"banana")
+    print(fruits1)
+
+if __name__=="__main__":
+    main()
