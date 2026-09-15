125d8feca959fc7521b3bbadf12daa20e9b1db94
aanaalvrzz1 <annesophia060909@gmail.com>
2026-09-15 17:27:52 +0000

    to-do work avance

---
+++ to-do.py
@@ -0,0 +1,23 @@
+def main():
+    tasks = []
+    while True:
+        print(f"You have {len(tasks)} task to do.")
+        print(tasks)
+        command = input("What do you want to do? (add, complete, or end): ").lower()
+        if command == "add":
+            new_task = input("Enter a new task: ")
+            tasks.append(new_task)
+        elif command == "stop":
+            break
+
+
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    main()
