ae0d0c228abc914d511733b2266a3da073f796f0
aanaalvrzz1 <annesophia060909@gmail.com>
2026-09-23 20:20:37 +0000

    avance de tables

---
+++ Weekly Checkpoint 6/tables.py
@@ -0,0 +1,17 @@
+def main():
+        list[1,2,3,4,5,6,7,8,9,10]
+        num = int(input("Enter a number (1-10) "))
+        while num >=10 and num >0 :
+            print(f"Here is the {num} times table")
+            for num in list:
+                print(f"{list} times {num} = {num*list}")
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    main()
